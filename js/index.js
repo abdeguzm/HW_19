0d7ab6fb2e79ca98227a3a10769cba542cc1fd05
Adrien D <adriendeguzman@gmail.com>
2019-08-08 19:51:28 -0700

    Added Google Analytics & Hotjar

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {origin: 'left', distance: '300px', duration: 2000});
+
+sr.reveal('.mr-3', {origin: 'left', distance: '300px', duration: 2000});
+
+sr.reveal('.media-body', { delay: 500 });
+
+sr.reveal('.cardContainer', { origin: 'bottom', distance: '200px', duration: 2000 });
+
+sr.reveal('.table', { origin: 'bottom', distance: '200px', duration: 2000 });
